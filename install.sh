2339785af52f645b92e98c5fd6549888b54dc931
jamesjwood <james.wood@datatransparency.com>
2018-10-29 11:19:25 +0000

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,15 @@
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install node
+npm -g install homebridge --unsafe-perm
+npm -g install homebridge-broadlink-rf
+npm -g install homebridge-nest
+npm -g install homebridge-weather-plus
+cp ./settings.json ~/.homebridge/settings.json
+
+cp com.homebridge.server.plist /Library/LaunchDaemons/com.homebridge.server.plist
+sudo chown $(whoami) ~/Library/LaunchAgents
+sudo chown $(whoami) ~/Library/LaunchAgents/com.homebridge.server.plist
+sudo chmod 700 ~/Library/LaunchAgents
+sudo chmod 600 ~/Library/LaunchAgents/com.homebridge.server.plist
+
+reboot
